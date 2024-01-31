09f7bc24e7da348fa2547eb1002f2a0f67d4a696
kdavis5111 <119846859+kdavis5111@users.noreply.github.com>
2024-01-31 10:30:58 -0700

    It won't let me pull unless I commit...

--- obj/Release/net8.0/Mission4.AssemblyInfo.cs
+++ obj/Release/net8.0/Mission4.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission4")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4641325dfc3ec8f35fc2311578bdd2387c74e2fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission4")]
